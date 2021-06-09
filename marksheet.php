5dc8aab5df9ce69793c149eaf067131e8008572d
shahsupan <84909057+shahsupan@users.noreply.github.com>
2021-06-09 23:10:12 +0530

    Add files via upload

--- marksheet.php
+++ marksheet.php
@@ -1,40 +1,109 @@
-<form method=post>
-			<center>
-			<table>
-				<tr> <td id="a">Social seince 1:-</td> <td id="b"><input type="number" name="num1"></td> </tr>
-				<tr> <td id="a">English 2:-</td> <td id="b"><input type="number" name="num2"></td> </tr>
-				<tr> <td id="a">Maths3:-</td> <td id="b"><input type="number" name="num3"></td> </tr>
-				<tr> <td colspan="2" id="c"><input type="submit" name="submit" value="Submit" id="submit"></td> </tr>
-			</table>
-			</center>
-		</form>
-		<?php
-			if(isset($_POST['submit'])){
-				$a = $_POST['num1'];
-				$b = $_POST['num2'];
-				$c = $_POST['num3'];
-				if($a>$b and $a>$c){
-					echo "Number 1 is Maximum : $a";
-				}
-				elseif ($b>$a and $b>$c) {
-					echo "Number 2 is Maximum : $b";
-				}
-				elseif ($c>$a and $c>$b) {
-					echo "Number 3 is Maximum : $c";
-				}
-				else{
-					echo "All values are equal";
-				}
-			}
-		?>
+<form>
+				<center><table>
+					<tr><td align="left">Enrolment Number:</td><td><input type="number" name="erno" minlength="12" maxlength="12" required></td></tr>
+					<tr><td align="left">Student Name:</td><td><input type="text" name="name"></td></tr>
+					<tr><td align="left">Maths:</td><td><input type="number" name="maths" required></td></tr>
+					<tr><td align="left">Physics:</td><td><input type="number" name="physics" required></td></tr>
+					<tr><td align="left">Chemistry:</td><td><input type="number" name="chemistry" required></td></tr>
+					<tr><td colspan="2"><input type="submit" name="submit"></td></tr>
+				</table></center><br/>
+				
+				<?php
+					if (isset($_GET['submit'])) {
+						$enrolment = $_GET['erno'];
+						$name = $_GET['name'];
+						$maths = $_GET['maths'];
+						$physics = $_GET['physics'];
+						$chemistry = $_GET['chemistry'];
+						$total = $maths + $physics + $chemistry;
+						$outof = 300;
+						$percentage = $total / $outof * 100; 
+						echo "$name <br/><br/>";
+						echo "$enrolment <br/><br/> ";
+				?>
+						<center><table border="1">
+							<tr>
+								<th>Subject</th>
+								<th>Marks</th>
+								<th>Remarks</th>
+								<th>Total</th>
+								<th>Percentage</th>
+							</tr>
+							<tr>
+								<td>Maths</td>
+								<td><?php echo "$maths"; ?></td>
+								<td>
+									<?php 
+										if ($maths == 100) {
+											echo "full marks";
+										} elseif ($maths > 85) {
+											echo "distinction";
+										} elseif($maths > 65) {
+											echo "good marks";
+										} elseif ($maths > 45) {
+											echo "average marks";
+										} elseif ($maths >= 23) {
+											echo "just pass";
+										} elseif ($maths < 23) {
+											echo "fail";
+										}
+									?>
+								</td>
+								<td rowspan="3"><?php echo "$total"; ?></td>
+								<td rowspan="3"><?php echo "$percentage"; ?></td>
+							</tr>
+							<tr>
+								<td>Physics</td>
+								<td><?php echo "$physics"; ?></td>
+								<td>
+									<?php 
+										if ($physics == 100) {
+											echo "full marks";
+										} elseif ($physics > 85) {
+											echo "distinction";
+										} elseif($physics > 65) {
+											echo "good marks";
+										} elseif ($physics > 45) {
+											echo "average marks";
+										} elseif ($physics >= 23) {
+											echo "just pass";
+										} elseif ($physics < 23) {
+											echo "fail";
+										}
+									?>
+								</td>
+							</tr>
+							<tr>
+								<td>Chemistry</td>
+								<td><?php echo "$chemistry"; ?></td>
+								<td>
+									<?php 
+										if ($chemistry == 100) {
+											echo "full marks";
+										} elseif ($chemistry > 85) {
+											echo "distinction";
+										} elseif($chemistry > 65) {
+											echo "good marks";
+										} elseif ($chemistry > 45) {
+											echo "average marks";
+										} elseif ($chemistry >= 23) {
+											echo "just pass";
+										} elseif ($chemistry < 23) {
+											echo "fail";
+										}
+									?>
+								</td>
+							</tr>
+						</table></center>
+						<?php	
+						//Total status
+						echo "<br/>";
 
-		<!DOCTYPE html>
-		<html>
-		<head>
-			<meta charset="utf-8">
-			<title>Marksheet</title>
-		</head>
-		<body>
-		
-		</body>
-		</html>
+						if ($percentage >= 35 && $maths >= 23 && $physics >= 23 && $chemistry >= 23) {
+							echo "The student has passed this exam ";
+						} else {
+							echo "The student has failed the exam ";
+						}
+					}
+				?>
+			</form>
